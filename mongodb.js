cd66ed40719d73b818a5bfd958c17cae0f5a04d5
devrolet <chetdhill@gmail.com>
2020-09-14 14:57:13 -0400

    Initial App Setup - Connect to MongoDB

---
+++ mongodb.js
@@ -0,0 +1,15 @@
+// DB CRUD operations
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'bukitlist2';
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+  if(error) {
+    return console.log('Unable to connect to database');
+  }
+
+  console.log('Connected to database successfully...');
+});
