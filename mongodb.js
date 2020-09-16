54826cd252148645c96e94188467df5d56e7467f
devrolet <chetdhill@gmail.com>
2020-09-16 11:47:59 -0400

    Delete Documents - MongoDB

--- mongodb.js
+++ mongodb.js
@@ -20,21 +20,14 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
   const db = client.db(databaseName);
 
-  // db.collection('users').updateOne({
-  //   _id: new ObjectID('5f5fc40a55ae6267d4bb41be')
-  // }, {
-  //   $inc: {
-  //     age: 1
-  //   }
+  // db.collection('users').deleteMany({
+  //   age: 23
   // }).then(result => console.log(result))
-  //   .catch(error => console.log(error));
-  
-  db.collection('tasks').updateMany({
-    completed: true
-  }, {
-    $set: {
-      completed: false
-    }
+  //   .catch(error => console.log(error)
+  // );
+
+  db.collection('tasks').deleteOne({
+    _id: new ObjectID('5f5fc40a55ae6267d4bb41c2')
   }).then((result) => {
     console.log(result);
   }).catch((error) => {
